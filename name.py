fedf45e63f9a02d3b31c732dda433d8cb68c4b0d
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-10 23:31:10 +0300

    Update name.py

--- name.py
+++ name.py
@@ -2,8 +2,8 @@ from field import Field
 
 
 class Name(Field):
-    """Represents a field for storing a name, inheriting from the Field class."""
+    # Represents a field for storing a name, inheriting from the Field class.
 
     def __init__(self, name):
-        """Initialize the Name field with a name."""
+        # Initialize the Name field with a name.
         self.value = name
